34589e1e27c54adf66c738862246e2e8dcb83c8d
João Vitor <joaovitorcfdev@gmail.com>
2025-06-28 16:21:27 -0300

    ensure NextEventLoader returns NextEvent on success

--- test/domain/usecases/next_event_loader_test.dart
+++ test/domain/usecases/next_event_loader_test.dart
@@ -1,29 +1,40 @@
 import 'dart:math';
 
+import 'package:advanced_flutter/domain/entities/next_event_player.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+class NextEvent {
+  final String groupName;
+  final DateTime date;
+  final List<NextEventPlayer> players;
+
+  NextEvent(this.players, {required this.groupName, required this.date});
+}
 
 class NextEventLoader {
   final LoadNextEventResository repo;
 
   NextEventLoader({required this.repo});
 
-  Future<void> call({required String groupId}) async {
-    await repo.loadNextEvent(groupId: groupId);
+  Future<NextEvent> call({required String groupId}) async {
+    return await repo.loadNextEvent(groupId: groupId);
   }
 }
 
 abstract class LoadNextEventResository {
-  Future<void> loadNextEvent({required String groupId});
+  Future<NextEvent> loadNextEvent({required String groupId});
 }
 
 class LoadNextEventMockResository implements LoadNextEventResository {
   String? groupId;
   var callCount = 0;
+  NextEvent? output;
 
   @override
-  Future<void> loadNextEvent({required String groupId}) async {
+  Future<NextEvent> loadNextEvent({required String groupId}) async {
     this.groupId = groupId;
     callCount++;
+    return output!;
   }
 }
 
@@ -35,6 +46,14 @@ void main() {
   setUp(() {
     groupId = Random().nextInt(50000).toString();
     repo = LoadNextEventMockResository();
+    repo.output = NextEvent(
+      [
+        NextEventPlayer(id: '1', name: 'John Doe', isConfirmed: true),
+        NextEventPlayer(id: '2', name: 'Jane Smith', isConfirmed: false),
+      ],
+      groupName: 'Test Group',
+      date: DateTime.now(),
+    );
     sut = NextEventLoader(repo: repo);
   });
 
@@ -42,5 +61,20 @@ void main() {
     await sut(groupId: groupId);
     expect(repo.groupId, groupId);
     expect(repo.callCount, 1);
+  });
+
+  test('should return event data on success', () async {
+    final event = await sut(groupId: groupId);
+    expect(event.groupName, repo.output?.groupName);
+    expect(event.date, repo.output?.date);
+    expect(event.players.length, 2);
+    expect(event.players[0].id, repo.output?.players[0].id);
+    expect(event.players[0].name, repo.output?.players[0].name);
+    expect(event.players[0].initials, isNotEmpty);
+    expect(event.players[0].isConfirmed, repo.output?.players[0].isConfirmed);
+    expect(event.players[1].id, repo.output?.players[1].id);
+    expect(event.players[1].name, repo.output?.players[1].name);
+    expect(event.players[1].initials, isNotEmpty);
+    expect(event.players[1].isConfirmed, repo.output?.players[1].isConfirmed);
   });
 }
