c5b4a242e9ff8945a915b9cbf1ac690cbb987f73
João Vitor <joaovitorcfdev@gmail.com>
2025-06-28 14:22:17 -0300

    move sut creation to a factory method

--- test/domain/entities/next_event_player_test.dart
+++ test/domain/entities/next_event_player_test.dart
@@ -19,15 +19,16 @@ class NextEventPlayer {
 
   String getInitials() {
     final names = name.split(' ');
-
-    return '${names[0][0]}${names[1][0]}';
+    final firstChar = names.first[0];
+    final lastChar = names.last[0];
+    return '$firstChar$lastChar';
   }
 }
 
 void main() {
+  NextEventPlayer makeSut(String name) => NextEventPlayer(id: '', name: name, isConfirmed: true);
   test('should return the first letter of the first and last names', () {
-    final player = NextEventPlayer(id: '', name: 'John Doe', isConfirmed: false);
-
-    expect(player.getInitials(), 'JD');
+    expect(makeSut('John Doe').getInitials(), 'JD');
+    expect(makeSut('John Doe Smith').getInitials(), 'JS');
   });
 }
