2bcd8321b57588fdc36fcfddb1e2876f4cd5f78e
João Vitor <joaovitorcfdev@gmail.com>
2025-07-01 18:32:39 -0300

    make LoadNextEventsHttpRepository implements LoadNextEventRepository

--- test/infra/repositories/load_next_event_http_repo_test.dart
+++ test/infra/repositories/load_next_event_http_repo_test.dart
@@ -3,17 +3,19 @@ import 'dart:typed_data';
 
 import 'package:advanced_flutter/domain/entities/next_event.dart';
 import 'package:advanced_flutter/domain/entities/next_event_player.dart';
+import 'package:advanced_flutter/domain/repositories/load_next_event_repo.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 
 import '../../helpers/fakes.dart';
 
-class LoadNextEventHttpRepository {
+class LoadNextEventHttpRepository implements LoadNextEventResository {
   final Client httpClient;
   final String url;
 
   LoadNextEventHttpRepository({required this.httpClient, required this.url});
 
+  @override
   Future<NextEvent> loadNextEvent({required String groupId}) async {
     final uri = Uri.parse(url.replaceFirst(':groupId', groupId));
     final headers = {'content-type': 'application/json', 'accept': 'application/json'};
