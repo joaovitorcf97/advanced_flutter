bda4ba87bf4d6c3e5989ca8aa2a55fc772d09668
João Vitor <joaovitorcfdev@gmail.com>
2025-07-01 21:22:18 -0300

    move LoadNextEventApiRepo to its own file

---
+++ lib/infra/api/repositories/load_next_event_api_repo.dart
@@ -0,0 +1,18 @@
+import '../../../domain/entities/next_event.dart';
+import '../../../domain/repositories/load_next_event_repo.dart';
+import '../../types/json.dart';
+import '../clients/http_get_client.dart';
+import '../mappers/next_event_mapper.dart';
+
+class LoadNextEventApiRepository implements LoadNextEventResository {
+  final HttpGetClient httpClient;
+  final String url;
+
+  LoadNextEventApiRepository({required this.httpClient, required this.url});
+
+  @override
+  Future<NextEvent> loadNextEvent({required String groupId}) async {
+    final json = await httpClient.get<Json>(url: url, params: {'groupId': groupId});
+    return NextEventMapper.toObject(json);
+  }
+}
