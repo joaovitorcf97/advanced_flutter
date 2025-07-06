cba6e69f96cd435c08902af723d43c7363885132
João Vitor <joaovitorcfdev@gmail.com>
2025-07-06 03:18:57 -0300

    change  HttpGetClient to accept non string params, headers and queryStrings

--- test/infra/api/clients/http_get_client_spy.dart
+++ test/infra/api/clients/http_get_client_spy.dart
@@ -1,20 +1,21 @@
 import 'package:advanced_flutter/infra/api/clients/http_get_client.dart';
+import 'package:advanced_flutter/infra/types/json.dart';
 
 final class HttpGetClientSpy implements HttpGetClient {
   String? url;
   int callCount = 0;
-  Map<String, String>? headers;
-  Map<String, String?>? params;
-  Map<String, String>? queryString;
+  Json? headers;
+  Json? params;
+  Json? queryString;
   dynamic response;
   Error? error;
 
   @override
   Future<T?> get<T>({
     required String url,
-    Map<String, String>? headers,
-    Map<String, String?>? params,
-    Map<String, String>? queryString,
+    Json? headers,
+    Json? params,
+    Json? queryString,
   }) async {
     this.url = url;
     this.headers = headers;
