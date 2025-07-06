787de59c9d6a05dbecc27d0c157405adabe7fcbc
João Vitor <joaovitorcfdev@gmail.com>
2025-07-06 02:03:43 -0300

    ensure HttpClient returns a null on 200 with empty response

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -16,7 +16,7 @@ class HttpClient {
     required this.client,
   });
 
-  Future<T> get<T>({
+  Future<T?> get<T>({
     required String url,
     Map<String, String>? headers,
     Map<String, String?>? params,
@@ -34,6 +34,7 @@ class HttpClient {
     switch (response.statusCode) {
       case 200:
         {
+          if (response.body.isEmpty) return null;
           final data = jsonDecode(response.body);
           return (T == JsonArr) ? data.map<Json>((e) => e as Json).toList() : data;
         }
@@ -180,8 +181,8 @@ void main() {
         } 
       ''';
       final data = await sut.get<Json>(url: url);
-      expect(data['key1'], 'value1');
-      expect(data['key2'], 'value2');
+      expect(data?['key1'], 'value1');
+      expect(data?['key2'], 'value2');
     });
 
     test('should return a List', () async {
@@ -192,8 +193,8 @@ void main() {
         ] 
       ''';
       final data = await sut.get<JsonArr>(url: url);
-      expect(data[0]['key1'], 'value1');
-      expect(data[1]['key2'], 'value2');
+      expect(data?[0]['key1'], 'value1');
+      expect(data?[1]['key2'], 'value2');
     });
 
     test('should return a Map with List', () async {
@@ -207,9 +208,15 @@ void main() {
         }
       ''';
       final data = await sut.get<Json>(url: url);
-      expect(data['key1'], 'value1');
-      expect(data['key2'][0]['key3'], 'value3');
-      expect(data['key2'][1]['key4'], 'value4');
+      expect(data?['key1'], 'value1');
+      expect(data?['key2'][0]['key3'], 'value3');
+      expect(data?['key2'][1]['key4'], 'value4');
+    });
+
+    test('should return null on 200 with empty response', () async {
+      client.responseJson = '';
+      final data = await sut.get(url: url);
+      expect(data, null);
     });
   });
 }
