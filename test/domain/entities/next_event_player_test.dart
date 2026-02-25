80d4d5a3d1d1801f3f0f76c6f9b95777ec862f0f
João Vitor <joaovitorcfdev@gmail.com>
2026-02-24 21:30:51 -0300

    return correct initials in a name with 2 words

---
+++ test/domain/entities/next_event_player_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+
+class NextEventPlayer {
+  final String id;
+  final String name;
+  final String? photo;
+  final String? position;
+  final bool isConfirmed;
+  final DateTime? confirmationDate;
+
+  NextEventPlayer({
+    required this.id,
+    required this.name,
+    this.photo,
+    this.position,
+    this.isConfirmed = false,
+    this.confirmationDate,
+  });
+
+  String getInitials() {
+    final names = name.split(' ');
+    return '${names[0][0]}${names[1][0]}';
+  }
+}
+
+void main() {
+  test('should return the first letter of the first and last name', () {
+    final player = NextEventPlayer(
+      id: '',
+      name: 'João Vitor',
+      isConfirmed: true,
+    );
+
+    expect(player.getInitials(), 'JV');
+  });
+}
